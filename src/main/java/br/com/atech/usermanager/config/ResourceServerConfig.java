d6f6c535734170b2c645c98b8790398b8845d867
Luiz Nicolau <luiznicolaulemosfreitas@gmail.com>
2022-08-28 18:37:25 -0300

    :recycle: updated endpoint paths

--- src/main/java/br/com/atech/usermanager/config/ResourceServerConfig.java
+++ src/main/java/br/com/atech/usermanager/config/ResourceServerConfig.java
@@ -13,7 +13,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/user").permitAll()
-                .antMatchers("/user").authenticated();
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .antMatchers("/users").authenticated();
     }
 }
