e0d8fb20ce6b174cb51ed81f5b3881ba91fa0b50
Luiz Nicolau <luiznicolaulemosfreitas@gmail.com>
2022-08-28 18:37:58 -0300

    :recycle: errorCode to errorMessage

--- src/main/java/br/com/atech/usermanager/dto/CreateUserDto.java
+++ src/main/java/br/com/atech/usermanager/dto/CreateUserDto.java
@@ -1,6 +1,6 @@
 package br.com.atech.usermanager.dto;
 
-import br.com.atech.usermanager.constant.ErrorCode;
+import br.com.atech.usermanager.constant.ErrorMessage;
 import br.com.atech.usermanager.model.Status;
 import lombok.Data;
 
@@ -12,16 +12,16 @@ import javax.validation.constraints.NotNull;
 
 @Data
 public class CreateUserDto {
-    @NotBlank(message = ErrorCode.NAME_IS_MANDATORY)
+    @NotBlank(message = ErrorMessage.NAME_IS_MANDATORY)
     private String name;
-    @NotBlank(message = ErrorCode.EMAIL_IS_MANDATORY)
-    @Email(message = ErrorCode.VALID_EMAIL_IS_REQUIRED)
+    @NotBlank(message = ErrorMessage.EMAIL_IS_MANDATORY)
+    @Email(message = ErrorMessage.VALID_EMAIL_IS_REQUIRED)
     private String email;
-    @NotBlank(message = ErrorCode.PASSWORD_IS_MANDATORY)
+    @NotBlank(message = ErrorMessage.PASSWORD_IS_MANDATORY)
     private String password;
-    @NotBlank(message = ErrorCode.USER_NAME_IS_MANDATORY)
+    @NotBlank(message = ErrorMessage.USER_NAME_IS_MANDATORY)
     private String userName;
-    @NotNull(message = ErrorCode.STATUS_IS_MANDATORY)
+    @NotNull(message = ErrorMessage.STATUS_IS_MANDATORY)
     @Enumerated(EnumType.ORDINAL)
     private Status status;
 }
